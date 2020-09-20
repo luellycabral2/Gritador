cb5bb1f2a37a0343e6421d671194a4f896ee433b
Luélly Cabral <luellycabral21@gmail.com>
2020-09-20 18:49:52 -0300

    Adiciona o grito do exercicio

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //<--- começo (digite)--->//
+            Console.Write(" DIGITE UM GRITO ");
+            string Grito = Console.ReadLine();
+            Console.Write("\n");
+            
+             Console.WriteLine("Grito......!!");
+            //<--- termino (digite)--->//
+           
+           
+            Console.WriteLine("clik a tecla ENTER para sair");
+            Console.ReadKey();
+        }
+    }
+}
